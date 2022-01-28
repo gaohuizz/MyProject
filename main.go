8a95cb75b11261b27b066609135d71da8313d6b7
高翔 <gaoxiang@gaoxiangdeMacBook-Pro.local>
2022-01-28 13:05:18 +0800

    origin push

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//for {
+	//	time.Sleep(1 * time.Second)
+	//	fmt.Println(time.Now())
+	//	fmt.Println(time.Now().Unix())
+	//}
+	//str := "成功"
+	//str2 := "失败"
+	//fmt.Printf("失败原因：%s 成功原因：%s", str2, str)
+	map1 := make(map[string]int)
+	map1["name"] += 1
+	fmt.Print(map1)
+	var a int64
+	fmt.Print(a == 0)
+}
